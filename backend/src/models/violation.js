227167e169efba670fa82ce92b92954f7e34414b
eternalbeginner <ini.dwiii@gmail.com>
2023-06-27 18:38:50 +0800

    Refactor to fix some bugs on violation model

--- backend/src/models/violation.js
+++ backend/src/models/violation.js
@@ -2,6 +2,14 @@ import prisma from 'libs/prisma';
 
 import ModelError from 'errors/ModelError';
 
+const VIOLATION_FIND_ALL_REL_USER_QUERY_OBJ = { id: true, regNumber: true, name: true };
+const VIOLATION_FIND_ONE_REL_USER_QUERY_OBJ = {
+  id: true,
+  regNumber: true,
+  name: true,
+  avatarUrl: true,
+};
+
 export async function findAll(filters = {}, errorOptions = {}) {
   try {
     const violations = await prisma.violation.findMany({
@@ -10,8 +18,8 @@ export async function findAll(filters = {}, errorOptions = {}) {
       },
       include: {
         category: true,
-        from: { select: { id: true, regNumber: true, name: true } },
-        to: { select: { id: true, regNumber: true, name: true } },
+        from: { select: VIOLATION_FIND_ALL_REL_USER_QUERY_OBJ },
+        to: { select: VIOLATION_FIND_ALL_REL_USER_QUERY_OBJ },
       },
     });
 
@@ -30,41 +38,49 @@ export async function findByViolationId(violationId, errorOptions = {}) {
       where: { id: violationId },
       include: {
         category: true,
-        from: { select: { id: true, regNumber: true, name: true, avatarUrl: true } },
-        to: { select: { id: true, regNumber: true, name: true, avatarUrl: true } },
+        from: { select: VIOLATION_FIND_ALL_REL_USER_QUERY_OBJ },
+        to: { select: VIOLATION_FIND_ONE_REL_USER_QUERY_OBJ },
       },
     });
 
     return violation;
   } catch {
-    throw new ModelError('No violation was found with the id provided', 404, {
+    throw new ModelError('No  violation was found with the id provided', 404, {
       ...errorOptions,
-      path: 'violationId',
+      path: ' violationId',
     });
   }
 }
 
 export async function create(fromId, toId, newViolation, errorOptions = {}) {
   try {
-    await prisma.violation.create({
-      data: {
-        title: newViolation.title,
-        description: newViolation.description,
-        points: newViolation.points,
-        tags: newViolation.tags,
-        from: { connect: { id: fromId } },
-        to: { connect: { id: toId } },
-        category: {
-          ...('categoryId' in newViolation
-            ? { connect: { id: newViolation.categoryId } }
-            : { create: newViolation.newCategory }),
+    await prisma.$transaction([
+      prisma.violation.create({
+        data: {
+          title: newViolation.title,
+          description: newViolation.description,
+          points: newViolation.points,
+          tags: newViolation.tags,
+          from: { connect: { id: fromId } },
+          to: { connect: { id: toId } },
+          category: {
+            ...('categoryId' in newViolation
+              ? { connect: { id: newViolation.categoryId } }
+              : { create: newViolation.newCategory }),
+          },
         },
-      },
-    });
+      }),
+      prisma.user.update({
+        data: {
+          obtainedViolationPoints: { increment: newViolation.points },
+        },
+        where: { id: toId },
+      }),
+    ]);
 
     return true;
   } catch {
-    throw new ModelError('Something went wrong while creating new violation', 500, {
+    throw new ModelError('Something went wrong while creating new  violation', 500, {
       ...errorOptions,
       type: 'create',
     });
@@ -73,47 +89,63 @@ export async function create(fromId, toId, newViolation, errorOptions = {}) {
 
 export async function updateByViolationId(violationId, updatedViolation = {}, errorOptions = {}) {
   try {
+    const oldViolationData = await prisma.violation.findFirstOrThrow({
+      where: { id: violationId },
+      select: { toId: true, points: true },
+    });
+
     const violationUpdatedData = {};
+    const violationUpdatedPoints = oldViolationData.points - (updatedViolation.points ?? 0);
 
-    if ('title' in updatedViolation) {
-      violationUpdatedData['title'] = updatedViolation.title;
-    }
+    ['categoryId', 'toId', 'title', 'description', 'points', 'tags'].forEach((col) => {
+      if (col in updatedViolation) {
+        violationUpdatedData[col] = updatedViolation[col];
+      }
+    });
 
-    if ('description' in updatedViolation) {
-      violationUpdatedData['description'] = updatedViolation.description;
-    }
+    if (!('categoryId' in updatedViolation) && 'newCategory' in updatedViolation) {
+      const newCategory = await prisma.category.create({
+        data: updatedViolation.newCategory,
+        select: { id: true },
+      });
 
-    if ('points' in updatedViolation) {
-      violationUpdatedData['points'] = updatedViolation.points;
-    }
-
-    if ('tags' in updatedViolation) {
-      violationUpdatedData['tags'] = updatedViolation.tags;
-    }
-
-    if ('toId' in updatedViolation) {
-      violationUpdatedData['to'] = { connect: { id: updatedViolation.toId } };
-    }
-
-    if ('categoryId' in updatedViolation || 'newCategory' in updatedViolation) {
-      violationUpdatedData['category'] = {};
-
-      if ('categoryId' in updatedViolation)
-        violationUpdatedData['category']['connect'] = { id: updatedViolation.categoryId };
-      else violationUpdatedData['category']['create'] = updatedViolation.newCategory;
+      violationUpdatedData['categoryId'] = newCategory.id;
     }
 
     violationUpdatedData['updatedAt'] = new Date();
 
-    await prisma.violation.update({
-      data: violationUpdatedData,
-      where: { id: violationId },
-    });
+    await prisma.$transaction([
+      prisma.violation.update({
+        data: violationUpdatedData,
+        where: { id: violationId },
+      }),
+      ...(updatedViolation.toId && updatedViolation.toId !== oldViolationData.toId
+        ? [
+            prisma.user.update({
+              data: { obtainedViolationPoints: { decrement: oldViolationData.points } },
+              where: { id: oldViolationData.toId },
+            }), // remove the old points from the old user
+            prisma.user.update({
+              data: {
+                obtainedViolationPoints: {
+                  increment: updatedViolation.points ?? oldViolationData.points,
+                },
+              },
+              where: { id: updatedViolation.toId },
+            }), // add the new updated points to the new user
+          ]
+        : [
+            prisma.user.update({
+              data: { obtainedViolationPoints: { decrement: violationUpdatedPoints } },
+              where: { id: oldViolationData.toId },
+            }),
+          ]),
+    ]);
 
     return true;
   } catch (err) {
     console.error(err);
-    throw new ModelError(`Something went wrong while updating violation #${violationId}`, 500, {
+    throw new ModelError(`Something went wrong while updating  violation #${violationId}`, 500, {
       ...errorOptions,
       type: 'update',
     });
@@ -122,13 +154,18 @@ export async function updateByViolationId(violationId, updatedViolation = {}, er
 
 export async function deleteByViolationId(violationId, errorOptions = {}) {
   try {
-    await prisma.violation.delete({
+    const deletedViolation = await prisma.violation.delete({
       where: { id: violationId },
+    });
+
+    await prisma.user.update({
+      data: { obtainedViolationPoints: { decrement: deletedViolation.points } },
+      where: { id: deletedViolation.toId },
     });
 
     return true;
   } catch {
-    throw new ModelError(`Something went wrong while updating violation #${violationId}`, 500, {
+    throw new ModelError(`Something went wrong while updating  violation #${violationId}`, 500, {
       ...errorOptions,
       type: 'delete',
     });
